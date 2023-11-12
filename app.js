51558d44b925e56956c3009150c8ce90233837e0
Muhamad Nur Syami <nursami385@gmail.com>
2023-11-12 07:50:06 +0700

    berhasil connect ke database MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const db = require("./config/db");
 const allRoutes = require("./routes");
 const PORT = process.env.PORT || 3000;
+
+db.then(() => {
+  console.log("Berhasil connect database MongoDB");
+}).catch(() => {
+  console.log("gagal connect ke database MongoDB");
+});
 
 app.use(cors());
 app.use(express.json());
